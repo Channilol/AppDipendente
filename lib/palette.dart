a9074fc48a7038bbb2c00096c5e9278fdfcd37f9
Channilol <fcannizzo97@gmail.com>
2025-08-25 23:51:40 +0200

    added dependencies, added gorouted setup, added palette static class

---
+++ lib/palette.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+class Palette {
+  static Color background = Color(0xFFFFFFFF);
+  static Color primary = Color(0xFFFFFFFF);
+  static Color hover = Color(0xFFFFFFFF);
+  static Color info = Color(0xFFFFFFFF);
+  static Color warning = Color(0xFFFFFFFF);
+  static Color danger = Color(0xFFFFFFFF);
+  static Color success = Color(0xFFFFFFFF);
+  static Color border = Color(0xFFFFFFFF);
+  static Color text = Color(0xFFFFFFFF);
+  static Color shadow = Color(0xFFFFFFFF);
+}
